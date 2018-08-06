25d2708df3d5cb4e93252219c632bf152571f6c8
Cruel-Octopus <mobilepocket358@gmail.com>
2018-08-06 18:31:28 +0400

    work in process

--- FlacHeader.h
+++ FlacHeader.h
@@ -4,19 +4,17 @@ typedef struct _MetadataBlockStreamInfo
 {
 	uint16_t	MinBlockSize;
 	uint16_t	MaxBlockSize;
-	uint32_t	MinFrameSize;
-	uint32_t	MaxFrameSize;
+	uint8_t		MinFrameSize[4];
+	uint8_t		MaxFrameSize[4];
 	uint32_t	SampleRateCHBit;
 	uint32_t	TotalSamplesInStream;
+	uint8_t		MD5[32];
 } MetadataBlockStreamInfo;
 
 typedef struct _FlacHeader
 {
-	
-    // (0x664C6143 в big-endian представлении "fLaC")
-	char	FlacId[4];
-	//uint32_t  MetadataBlockHeader;
-	char	MetadataBlockHeader[4];
+	char	FlacId[4];		// (0x664C6143 в big-endian представлении "fLaC")
+	char	MetadataBlockHeader[4];		//uint32_t  MetadataBlockHeader;
 	MetadataBlockStreamInfo	MBSI;
 } FlacHeader;
 
--- FlacTest.cpp
+++ FlacTest.cpp
@@ -18,6 +18,7 @@ typedef struct _StreamInfo{
 	uint8_t		Chanels;
 	uint8_t		BitsPerSample;
 	uint64_t	TotalSamplesInStream;
+	uint8_t		MD5[32];
 }StreamInfo;
 
 class FlacDecoder{
@@ -26,22 +27,17 @@ class FlacDecoder{
 		StreamInfo SI;
 };
 
-static uint32_t reverseBits(uint32_t n) {
-        //n = (n >> 1) & 0x55555555 | (n << 1) & 0xaaaaaaaa;
-        //n = (n >> 2) & 0x33333333 | (n << 2) & 0xcccccccc;
-        n = (n >> 4) & 0x0f0f0f0f | (n << 4) & 0xf0f0f0f0;
-        n = (n >> 8) & 0x00ff00ff | (n << 8) & 0xff00ff00;
-        n = (n >> 16) & 0x0000ffff | (n << 16) & 0xffff0000;
-        return n;
-    }
+
 int main(array<System::String ^> ^args)
 {
 	FlacDecoder FD;
+	void * Temp;
+	uint32_t TempData=0;
 
-	char *fileName = "c:\\stereo.flac";
+	char *fileName = "c:\\Channel Test (5.0).flac";
     FILE *FlacFile = fopen(fileName, "rb");
 	FlacHeader FHEADER;
-
+	int o =sizeof(FlacHeader);
 	fread(&FHEADER, sizeof(FHEADER), 1, FlacFile);
     unsigned t=0;
 	if((FHEADER.FlacId[0]==0x66)&&
@@ -59,17 +55,46 @@ int main(array<System::String ^> ^args)
 			switch (FHEADER.MetadataBlockHeader[0]&0x7f)		//get type of block
 			{
 				case 0:{
-							FHEADER.MetadataBlockHeader[0]=0x00;
-							void * temp = FHEADER.MetadataBlockHeader;
-							uint32_t td = *(uint32_t *)temp; 
-							td = _byteswap_ulong(td);
+							//FHEADER.MetadataBlockHeader[0]=0x00;
+							//void * Temp = FHEADER.MetadataBlockHeader;
+							//uint32_t TempData = *(uint32_t *)Temp; 
+							//td = _byteswap_ulong(td);
+
 							FD.SI.MinBlockSize = _byteswap_ushort(FHEADER.MBSI.MinBlockSize);
 							FD.SI.MaxBlockSize = _byteswap_ushort(FHEADER.MBSI.MaxBlockSize);
-							FD.SI.MinFrameSize = _byteswap_ulong(FHEADER.MBSI.MinFrameSize);
-							FD.SI.MaxFrameSize = _byteswap_ulong(FHEADER.MBSI.MaxFrameSize);
-							FD.SI.SampleRate = FHEADER.MBSI.SampleRateCHBit;
-							FD.SI.SampleRate = FD.SI.SampleRate>>4;
+							
+							Temp = FHEADER.MBSI.MinFrameSize;
+							FD.SI.MinFrameSize = *(uint32_t *)Temp;
+							FD.SI.MinFrameSize = FD.SI.MinFrameSize << 8;
+							FD.SI.MinFrameSize = _byteswap_ulong(FD.SI.MinFrameSize);
+
+							Temp = &FHEADER.MBSI.MinFrameSize[3];
+							FD.SI.MaxFrameSize = *(uint32_t *)Temp;
+							FD.SI.MaxFrameSize = FD.SI.MaxFrameSize << 8;
+							FD.SI.MaxFrameSize = _byteswap_ulong(FD.SI.MaxFrameSize);
+							
+							Temp = &FHEADER.MBSI.MaxFrameSize[2];
+							FD.SI.SampleRate = *(uint32_t *)Temp;
 							FD.SI.SampleRate = _byteswap_ulong(FD.SI.SampleRate);
+
+							FD.SI.SampleRate = FD.SI.SampleRate >> 0x0c;
+
+							TempData = *(uint32_t *)Temp;
+							TempData = _byteswap_ulong(TempData);
+							TempData = TempData >> 4;
+							TempData = TempData & 0x000000ff;
+							FD.SI.Chanels = TempData >> 5;
+							FD.SI.BitsPerSample = TempData & 0x1f;
+
+							FD.SI.TotalSamplesInStream = *(uint64_t *)Temp;
+							FD.SI.TotalSamplesInStream = _byteswap_uint64(FD.SI.TotalSamplesInStream);
+							FD.SI.TotalSamplesInStream = FD.SI.TotalSamplesInStream & 0x0000000fffffffff;
+
+							Temp = FHEADER.MBSI.MD5;
+							static_cast (uint8_t *)Temp;
+							Temp--;
+							Temp--;
+
 					};	//STREAMINFO 
 			}
 		}while(IsLastMetadataBlock!=1);
